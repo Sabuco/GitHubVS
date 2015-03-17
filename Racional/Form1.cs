c8a1f98012d23162eb35a4f32c8902bc36fce62d
Sabuco <alvarogs56@gmail.com>
2015-03-17 19:32:14 +0100

    adsf

---
+++ Racional/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Racionales.Model;
+
+namespace Racionales
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multipl_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt16(textBox1.Text);
+            int d1 = Convert.ToInt16(textBox2.Text);
+            int n2 = Convert.ToInt16(textBox3.Text);
+            int d2 = Convert.ToInt16(textBox4.Text);
+
+            Racional r1 = new Racional(n1, d1);
+            Racional r2 = new Racional(n2, d2);
+
+            Racional producto = r1.multiplicar(r2);
+
+            textBox5.Text = producto.getNumerador().ToString();
+            textBox6.Text = producto.getDenominador().ToString();
+        }
+    }
+}
